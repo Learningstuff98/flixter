53a86282977986af33d5d56536cc07c63839c1a8
Andy Strube <andystrube64@gmail.com>
2019-05-04 18:24:43 -0700

    Lesson 28 complete

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
   end
@@ -8,6 +10,16 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_authorized_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You must enroll in this course first"
+    end
+  end
+
+  def current_course
+    @current_course ||= Course.find(params[:id])
   end
 
 end
